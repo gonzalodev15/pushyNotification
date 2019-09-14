8c3a4541da141abbb23efa504727c857469108e1
gonzaloleonsuarez <u201513219@upc.edu.pe>
2019-09-14 14:03:30 -0500

    Large icon implemented

--- app/src/main/java/com/pe/me/pushnotification/PushReceiver.java
+++ app/src/main/java/com/pe/me/pushnotification/PushReceiver.java
@@ -5,14 +5,24 @@ import android.app.PendingIntent;
 import android.content.BroadcastReceiver;
 import android.content.Context;
 import android.content.Intent;
+import android.graphics.Bitmap;
 import android.graphics.Color;
 import android.media.RingtoneManager;
-
 import androidx.core.app.NotificationCompat;
+
+import com.bumptech.glide.Glide;
+import com.bumptech.glide.request.FutureTarget;
+import com.squareup.picasso.Picasso;
+import com.squareup.picasso.Target;
+
+import java.util.concurrent.ExecutionException;
 
 import me.pushy.sdk.Pushy;
 
 public class PushReceiver extends BroadcastReceiver {
+
+    Bitmap bitmap;
+
     @Override
     public void onReceive(Context context, Intent intent) {
         String notificationTitle = "MyApp";
@@ -23,6 +33,19 @@ public class PushReceiver extends BroadcastReceiver {
             notificationText = intent.getStringExtra("message");
         }
 
+        FutureTarget futureTarget = Glide.with(context)
+                .asBitmap()
+                .load("https://cdn3.iconfinder.com/data/icons/fillies-large/64/bell-512.png")
+                .submit();
+
+        try {
+            bitmap = (Bitmap) futureTarget.get();
+        } catch (ExecutionException e) {
+            e.printStackTrace();
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        }
+
         // Prepare a notification with vibration, sound and lights
         NotificationCompat.Builder builder = new NotificationCompat.Builder(context)
                 .setAutoCancel(true)
@@ -31,8 +54,10 @@ public class PushReceiver extends BroadcastReceiver {
                 .setContentText(notificationText)
                 .setLights(Color.RED, 1000, 1000)
                 .setVibrate(new long[]{0, 400, 250, 400})
+                .setLargeIcon(bitmap)
                 .setSound(RingtoneManager.getDefaultUri(RingtoneManager.TYPE_NOTIFICATION))
-                .setContentIntent(PendingIntent.getActivity(context, 0, new Intent(context, MainActivity.class), PendingIntent.FLAG_UPDATE_CURRENT));
+                .setContentIntent(PendingIntent.getActivity(context, 0, new Intent(context, MainActivity.class), PendingIntent.FLAG_UPDATE_CURRENT))
+                .setPriority(NotificationCompat.PRIORITY_MAX);
 
         // Automatically configure a Notification Channel for devices running Android O+
         Pushy.setNotificationChannel(builder, context);
@@ -43,4 +68,5 @@ public class PushReceiver extends BroadcastReceiver {
         // Build the notification and display it
         notificationManager.notify(1, builder.build());
     }
+
 }
